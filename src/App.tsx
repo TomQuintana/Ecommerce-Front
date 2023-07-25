55b6aba12f55d22d5a3bbca8c98c69c358755b36
tomasquintana-gj <tomas.quintana@gojiraf.com>
2023-07-25 17:40:23 -0300

    feat/declare routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import AuthLayout from './layout/authLayout'
+import NavigatePage from './pages/NavigatePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className='text-6xl'>
-        Hola 
-      </div>
-    </>
+  <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<AuthLayout />}>
+          <Route index element={<NavigatePage/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
