2fd24223bcc56bd2f6fded7e9bd3c51b8c39a046
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-02 00:21:13 -0300

    add cart component for show cart and cartIcon

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,40 +1,52 @@
-import {useContext} from 'react'
-import { MagnifyingGlassIcon, ShoppingBagIcon, UsersIcon } from '@heroicons/react/24/outline'
-import CartProductsContext from '../Context/CartProductsProvider'
+import useCart from '../hooks/useCart'
+import './Cart.css'
+
+const CartIcon = ({ name, price, img }) => {
+  return (
+    <>
+      <div className='p-8 mt-2'>
+        <h1>Producto: {name}</h1>
+        <p>Price: ${price}</p>
+        <p>Cantidad: 1</p>
+      </div>
+      <img
+        className='w-6/12 rounded-xl mt-2'
+        src={img}
+      /> 
+    </>
+  )
+}
 
 const Cart = () => {
 
-  const carrito = localStorage.getItem('Cart')
-  console.log(carrito);
-  
-  const carritoBest = JSON.parse(carrito)
-  console.log(carritoBest.length);
-
-  let carritoFinal = []
-
-  carritoBest.map((element) => {
-    console.log(element);
-    carritoFinal.push(element)
-    
-  })
-
-  console.log(carritoFinal);
-  
-
+  const { cartItems } = useCart()
 
   return (
-    <div>
-      <a href="/cart" className="group -m-2 flex items-center p-2">
-        <ShoppingBagIcon
-          className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
-          aria-hidden="true"
-        />
-        <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{carritoBest.length}
-        </span>
-        <span className="sr-only">items in cart, view bag</span>
-      </a>
+    <div className='flex justify-center'>
+      <div className='mt-8 rounded shadow w-6/12 h-max'>
+        <h1 className='flex justify-center font-mono text-2xl'>Titulo</h1>
+        <div className='mt-2 mb-4'>
+          <div className='grid grid-cols-2 mt-2'>
+            {cartItems.map((element:any) => 
+              <CartIcon 
+                name={element.name}
+                price={element.price}
+                img={element.img}
+              />
+            )}
+          </div>
+        </div>
+
+        { /* TODO: calcular total */ }
+        <div>Total: $x</div>
+
+        <div className='flex justify-center p-2'>
+          <button className='rounded-xl bg-blue-500 w-28 text-white'>Pagar</button>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default Cart
+
