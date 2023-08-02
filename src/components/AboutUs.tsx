14b1093f029f356fb3f988ab97b0e97b4e20f495
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-02 00:11:29 -0300

    add about component

---
+++ src/components/AboutUs.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Necessitatibus, earum consectetur voluptate error exercitationem officia repellat 
+        cum rem nulla illum consequuntur dolore, voluptas accusantium aperiam aspernatur? Quod magni expedita natus?</div>
+  )
+}
+
+export default AboutUs
