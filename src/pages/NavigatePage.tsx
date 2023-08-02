3b184bbb77d0398c81dfe7ba7078826bafe9072c
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-02 00:17:21 -0300

    change label a for link

--- src/pages/NavigatePage.tsx
+++ src/pages/NavigatePage.tsx
@@ -10,61 +10,78 @@ import Cart from './Cart'
 
 const NavigatePage = () => {
 
-  const {texts} = textsPromo
+  const { cartItems } = useCart()
 
+  const { texts } = textsPromo
   const promo = texts.PromoSingle
 
   return (
     <>
       <header className='relative bg-white font-mono'>
-        {promo !== ''? <Promo
+        {promo !== '' ? <Promo
           text={promo}
-        />: ''}
+        /> : ''}
         <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between border-b border-gray-200">
             <div className='flex justify-around gap-3 p-2'>
 
               {/* Products */}
               <div>
-                <Link 
+                <Link
                   className=""
-                  to="/">Productos 
-                </Link> 
+                  to="/">Productos
+                </Link>
               </div>
-              
+
               <p>|</p>
 
               {/* About us*/}
               <div>
-                Quienes somos ?
+                <Link
+                  className=""
+                  to="/about">
+                  Quienes somos ?
+                </Link>
+
               </div>
             </div>
 
             <div className='flex justify-start p-2'>
-              { /* TODO: falta pegarle al back para validar los datos */ }
-            {/* Sign in */}
+              { /* TODO: falta pegarle al back para validar los datos */}
+              {/* Sign in */}
               <div className='flex justify-between gap-1'>
                 <p className=''>Iniciar Sesion
                 </p>
-                <Link 
+                <Link
                   className=""
-                  to="/login"> 
+                  to="/login">
                   <span className="sr-only">Iniciar Sesion</span>
-                  <UsersIcon className="h-6 w-6" aria-hidden="true" /> 
-                </Link> 
+                  <UsersIcon className="h-6 w-6" aria-hidden="true" />
+                </Link>
               </div>
 
               {/* Search */}
               <div className="lg:ml-6">
-                <a href="#" className=" hover:text-gray-500">
+                <a href="#" className="">
                   <span className="sr-only">Search</span>
-                  <MagnifyingGlassIcon className="h-6 w-6"/>
+                  <MagnifyingGlassIcon className="h-6 w-6 hover:scale-110" />
                 </a>
               </div>
 
               {/* Cart */}
-              <div className="lg:ml-6">
-                <Cart  />
+              <div className="flex justify-center">
+                <Link
+                  to="/cart"
+                >
+                  <div>
+                    <ShoppingBagIcon
+                      className="h-6 w-6 flex-shrink-0 hover:scale-110 ml-5"
+                      aria-hidden="true"
+                    />
+                  </div>
+                </Link>
+                <div className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">{cartItems.length}
+                </div>
               </div>
             </div>
           </div>
