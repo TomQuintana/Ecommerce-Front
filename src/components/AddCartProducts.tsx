b59e8ad88d7438f559a450ae69f2a924c301320b
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-03 14:58:44 -0300

    change css

--- src/components/AddCartProducts.tsx
+++ src/components/AddCartProducts.tsx
@@ -25,7 +25,7 @@ const AddCartProducts = ({product}) => {
         onClick={() => addToCart(product)}
         type="submit" 
         value="Agregar al Carrito"
-        className='bg-indigo-700 w-full py-3 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 
+        className='bg-indigo-700 w-full py-3 rounded-xl text-white uppercase font-bold text-sm mt-5 hover:cursor-pointer hover:bg-indigo-800 
         md:w-auto px-10'
       >Agregar al Carrito
       </button>
