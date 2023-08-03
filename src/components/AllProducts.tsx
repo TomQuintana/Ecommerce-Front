2874f371cafc3da3fe518c5fed967d5f036ca44d
tomasquintana-gj <tomquintana20@gmail.com>
2023-08-03 15:00:07 -0300

    add stock and select for quantity

--- src/components/AllProducts.tsx
+++ src/components/AllProducts.tsx
@@ -9,15 +9,22 @@ const AllProducts = () => {
     <>
       <div className='grid grid-cols-3 items-center gap-8 container mx-auto font-mono'>
         {productos.map((producto, index) => 
-          <div className='border shadow-black  px-4 py-16'>
-            <img 
-              src={producto.img}
-            />
-            <div className='flex justify-between mt-2'>
-              <h1 key={index}>{producto.name}</h1>
+          <div className='grid grid-cols-1 border'>
+            <div className='flex justify-center'>
+              <img 
+                className='w-6/12 rounded-xl mt-2 flex justify-center'
+                src={producto.img}
+              />
+            </div>
+            <div className='flex justify-around mt-4'>
+              <h1 key={index}>Producto: {producto.name}</h1>
               <p key={index}>${producto.price}</p>
             </div>
-            <div className='flex justify-center'>
+            <div className='flex justify-around ml-6'>
+              <p key={index}>Stock: {producto.stock}</p>
+              <p >Cantidad - Select</p>
+            </div>
+            <div className='flex justify-center mb-2'>
               <AddCartProducts 
                 product={producto}
               />
